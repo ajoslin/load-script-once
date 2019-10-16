76acf2395a6b36e36231e5cb3012262c06696b63
오승환 <oshxios@gmail.com>
2019-10-16 22:25:11 +0900

    Type declaration with generic

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,6 @@
-declare function loadScriptOnce<C = () => any>(src: string, callback?: C): Promise<C>;
+type loadCallback<T = void> = (...args: any[]) => T;
+
+declare function loadScriptOnce<T = void>(src: string, callback?: loadCallback<T>): Promise<T>;
 
 declare module "load-script-once" {
     export default loadScriptOnce;
